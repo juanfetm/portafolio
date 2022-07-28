ca8246f59f504c31ed5c04da37a43173167b7724
Juan Felipe Torralba Montes <felipemontesjt200@gmail.com>
2022-07-27 22:09:53 -0500

    add page

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/spa-developer-portafolio/' : '/',
-
+  publicPath: '/portafolio/',
 });
