5db07d5cf9606d9c2722b304318d9d80c7a3099a
Juan Felipe Torralba Montes <felipemontesjt200@gmail.com>
2022-07-27 21:49:46 -0500

    init the proyec

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Vue</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    // HelloWorld,
+  },
+});
+</script>
+
+<style lang="css">
+@import "../scss/main.css";
+</style>
