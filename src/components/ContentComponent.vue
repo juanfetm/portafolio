7768815fab1c4fc2bbded7b05b6de0c0393c38d9
Juan Felipe Torralba Montes <felipemontesjt200@gmail.com>
2022-08-09 19:58:59 -0500

    arrange components and footer

---
+++ src/components/ContentComponent.vue
@@ -0,0 +1,59 @@
+<template>
+     <b-navbar toggleable="lg" type="dark" variant="danger"> </b-navbar>
+  <b-container>
+  <b-navbar toggleable="lg" type="dark" variant="danger">
+     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle></b-navbar>
+     </b-container>
+<div class="container-center">
+  <div class="card">
+      <theme-button />
+<div class="I" >
+<img alt="Vue logo" class="perfil" src="../assets/logo.png">
+<div>
+</div>
+  <h1>Hello, I´m Juanfe</h1>
+<p>
+I am a junior front-end developer. It seems to me that the world of technolog
+It is interesting and extensive because the
+job opportunities it offers are good and growing
+professionally day by day helps us to collaborate in
+this collaborative environment.</p>
+</div>
+<div class="A" >
+  <h1>About me</h1>
+<p>Since I was little I have found computer systems interesting, so I decided to study
+a computer systems technician in my city (Pensilvania-Caldas)
+and take different web development courses to expand
+my knowledge, to be able to contribute to society, collaborate to build it in a better way.</p>
+</div>
+<div class="P" >
+<h1>Proyects</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Illo iste hic, pariatur fugiat esse beatae velit.
+   >Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Illo iste hic, pariatur fugiat esse beatae veli</p>
+</div>
+  <div class="E" >
+  <h1>Estudies</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Illo iste hic, pariatur fugiat esse beatae velit.
+   >Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Illo iste hic, pariatur fugiat esse beatae veli</p>
+</div>
+
+  <div class="T" >
+<h1>Testimonials</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Illo iste hic, pariatur fugiat esse beatae velit.
+   >Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Illo iste hic, pariatur fugiat esse beatae veli</p>
+</div>
+</div>
+</div>
+</template>
+
+<script lang="ts">
+export default ({
+  name: 'ContentComponent',
+});
+</script>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer>
+   <div>
+          <h1>C ontact:</h1>
+          <br>
+          <b>Phone: (+57)3224632668</b>
+          <br>
+          <b>Mail: felipemontesjt200@gmail.com</b>
+          <br>
+          <b>Address: Cra. 8 50, Pensilvania, Caldas</b>
+      </div>
+</footer>
+</template>
+
+<script lang="ts">
+export default ({
+  name: 'FooterComponent',
+});
+</script>
