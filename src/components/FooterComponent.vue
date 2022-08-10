1fb1a448a91957e6c5604576b4dc6d4c15d32584
Juan Felipe Torralba Montes <felipemontesjt200@gmail.com>
2022-08-09 21:02:53 -0500

    links

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,13 +1,18 @@
 <template>
     <footer>
-   <div>
+      <div class="container">
+ <div class="contact">
           <h1>Contact:</h1>
-          <br>
-          <b>Phone: (+57)3224632668</b>
-          <br>
-          <b>Mail: felipemontesjt200@gmail.com</b>
-          <br>
-          <b>Address: Cra. 8 50, Pensilvania, Caldas</b>
+          <p>Phone: (+57)3224632668</p>
+          <p>Mail: felipemontesjt200@gmail.com</p>
+          <p>Address: Cra. 8 50, Pensilvania, Caldas</p>
+     </div>
+     <div class="ico">
+     <a href="https://www.linkedin.com/in/juan-felipe-torralba-montes/"
+   target="_blank"><img class="Linke" src="../assets/Linkedin.png" alt="Linkedin">
+    </a><a href="https://github.com/juanfetm"
+    target="_blank"><img class="github" src="../assets/github.png" alt="Github"></a>
+     </div>
       </div>
 </footer>
 </template>
